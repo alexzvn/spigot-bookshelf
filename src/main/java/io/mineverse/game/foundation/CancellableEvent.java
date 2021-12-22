888271f466eb990b00bb51f8418efb6dc5f71e57
Alex <41188285+alexzvn@users.noreply.github.com>
2021-12-22 05:34:48 +0000

    add cancellable event base

---
+++ src/main/java/io/mineverse/game/foundation/CancellableEvent.java
@@ -0,0 +1,22 @@
+package io.mineverse.game.foundation;
+
+import org.bukkit.event.Cancellable;
+
+public abstract class CancellableEvent extends Event implements Cancellable {
+    protected boolean cancelled = false;
+
+    /**
+     * Call the logic of event if it is not cancelled.
+     */
+    abstract public void finish();
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
