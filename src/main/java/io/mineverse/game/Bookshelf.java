be3c4f2f04b237880b54369cced2215af05d86af
Alex <41188285+alexzvn@users.noreply.github.com>
2021-12-22 05:35:13 +0000

    add singleton intance

--- src/main/java/io/mineverse/game/Bookshelf.java
+++ src/main/java/io/mineverse/game/Bookshelf.java
@@ -3,14 +3,20 @@ package io.mineverse.game;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bookshelf extends JavaPlugin {
-    
+
+    protected static Bookshelf instance;
+
     @Override
     public void onEnable() {
-        // TODO Auto-generated method stub
+        instance = this;
     }
 
     @Override
     public void onDisable() {
         // TODO Auto-generated method stub
+    }
+
+    public static Bookshelf getInstance() {
+        return instance;
     }
 }
