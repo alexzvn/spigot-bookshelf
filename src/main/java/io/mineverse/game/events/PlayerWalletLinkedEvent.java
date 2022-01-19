b82b6e38c45ec5da5b6e90b52a4b65f6b0767da9
Alex <41188285+alexzvn@users.noreply.github.com>
2022-01-19 08:35:36 +0000

    fix dashed uuid

--- src/main/java/io/mineverse/game/events/PlayerWalletLinkedEvent.java
+++ src/main/java/io/mineverse/game/events/PlayerWalletLinkedEvent.java
@@ -1,28 +1,26 @@
 package io.mineverse.game.events;
 
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
 import io.mineverse.game.foundation.Event;
+import io.mineverse.game.utils.Util;
 
 public class PlayerWalletLinkedEvent extends Event {
 
-    private Player player;
+    private String player;
     private String wallet;
 
     public PlayerWalletLinkedEvent(String player_uuid, String wallet) {
-        this.player = Bukkit.getPlayer(UUID.fromString(player_uuid));
+        this.player = player_uuid;
         this.wallet = wallet;
     }
 
     public Player getPlayer() {
-        return player;
+        return Bukkit.getPlayer(Util.dashedUUID(player));
     }
 
     public Boolean isPlayerOffline() {
-        return player == null;
+        return getPlayer().isOnline() == false;
     }
 
     public String getWalletAddress() {
