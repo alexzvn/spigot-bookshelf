43d7a0bce6af0ad0556812b24b249191ee7bada5
Alex <41188285+alexzvn@users.noreply.github.com>
2022-01-19 04:57:23 +0000

    fix required static method

--- src/main/java/io/mineverse/game/foundation/Event.java
+++ src/main/java/io/mineverse/game/foundation/Event.java
@@ -2,12 +2,16 @@ package io.mineverse.game.foundation;
 
 import org.bukkit.event.HandlerList;
 
-abstract public class Event extends org.bukkit.event.Event {
+public abstract class Event extends org.bukkit.event.Event {
 
-    protected HandlerList handlers = new HandlerList();
+    protected static HandlerList handlers = new HandlerList();
 
     @Override
     public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
         return handlers;
     }
 }
