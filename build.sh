934db96e60d5dc3cd5178e0f1cf163113c1224f7
Alex <41188285+alexzvn@users.noreply.github.com>
2022-01-26 09:14:46 +0000

    update new version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # default is home/codespace/.java/current/bin/java
 rm test/plugins/Bookshelf.jar
 mvn clean && mvn install
-cp target/Bookshelf-v0.0.3.jar test/plugins/Bookshelf.jar
+cp target/Bookshelf-v0.0.4.jar test/plugins/Bookshelf.jar
