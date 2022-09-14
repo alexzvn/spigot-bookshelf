17f2686ed455b79b80c0a1ecc2d7c933d068a0f2
Alex <41188285+alexzvn@users.noreply.github.com>
2022-09-14 16:21:46 +0000

    add new command library

--- src/main/java/io/mineverse/game/Bookshelf.java
+++ src/main/java/io/mineverse/game/Bookshelf.java
@@ -1,10 +1,19 @@
 package io.mineverse.game;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.jonahseguin.drink.CommandService;
+import com.jonahseguin.drink.Drink;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import io.mineverse.game.meta.GameMeta;
 import io.mineverse.game.utils.Config;
 import io.mineverse.game.utils.Instance;
+import io.mineverse.game.utils.Log;
 import io.socket.client.Socket;
 
 public class Bookshelf extends JavaPlugin {
@@ -15,11 +24,17 @@ public class Bookshelf extends JavaPlugin {
 
     protected EventRegister eventRegister;
 
+    protected Connection rabbitConnection;
+
+    protected CommandService cmd;
+
+    protected GameMeta gameMeta;
+
     public Bookshelf() {
         this.saveDefaultConfig();
 
         instance = this;
-
+        cmd = Drink.get(this);
         eventRegister = new EventRegister();
 
         BookshelfAPI.setInstance(new BookshelfAPI(this));
@@ -29,7 +44,22 @@ public class Bookshelf extends JavaPlugin {
     public void onEnable() {
         bindSocket();
 
-        new GameMeta().bind();
+        cmd.register(new BookshelfCommand(), "bookshelf", "bs");
+
+        try {
+            rabbitConnection = createRabbitConnection();
+        } catch (Exception e) {
+            Log.error("RabbitMQ connection failed to initialize!!!");
+            Log.error(e.getMessage());
+        }
+
+        gameMeta = new GameMeta();
+
+        gameMeta.bind();
+
+        cmd.registerCommands();
+
+        Log.info("Bookshelf has been enabled!");
     }
 
     @Override
@@ -39,6 +69,20 @@ public class Bookshelf extends JavaPlugin {
         if (socket != null) {
             socket.disconnect();
         }
+
+        if (rabbitConnection != null) {
+            try {
+                rabbitConnection.close();
+            } catch (IOException e) {
+                Log.error(e.getMessage());
+            }
+        }
+
+        rabbitConnection = null;
+    }
+
+    public CommandService getCommandService() {
+        return cmd;
     }
 
     protected void bindSocket() {
@@ -49,6 +93,28 @@ public class Bookshelf extends JavaPlugin {
         eventRegister.bind(socket, Config.getString("bookshelf.token"));
 
         socket.connect();
+    }
+
+    protected Connection createRabbitConnection() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost(Config.getString("rabbitmq.host"));
+        factory.setPort(Config.getInt("rabbitmq.port"));
+
+        if (Config.getBoolean("rabbitmq.use_auth")) {
+            factory.setUsername(Config.getString("rabbitmq.username"));
+            factory.setPassword(Config.getString("rabbitmq.password"));
+        }
+
+        return factory.newConnection();
+    }
+
+    public Connection getRabbitConnection() {
+        return rabbitConnection;
+    }
+
+    public GameMeta getGameMeta() {
+        return gameMeta;
     }
 
     public EventRegister getEventRegister() {
---
+++ src/main/java/io/mineverse/game/BookshelfCommand.java
@@ -0,0 +1,15 @@
+package io.mineverse.game;
+
+import org.bukkit.command.CommandSender;
+
+import com.jonahseguin.drink.annotation.Command;
+import com.jonahseguin.drink.annotation.Sender;
+
+public class BookshelfCommand {
+
+    @Command(name = "", aliases = {}, desc = "Root command", usage = "")
+    public void root(@Sender CommandSender sender) {
+        
+    }
+
+}
