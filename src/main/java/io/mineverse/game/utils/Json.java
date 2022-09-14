5275265452ffe2acbf2452b8d212c62077e2d3e3
Alex <41188285+alexzvn@users.noreply.github.com>
2022-09-14 16:20:44 +0000

    add helper convert json

--- src/main/java/io/mineverse/game/utils/Json.java
+++ src/main/java/io/mineverse/game/utils/Json.java
@@ -1,5 +1,6 @@
 package io.mineverse.game.utils;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -7,6 +8,16 @@ public class Json {
 
     public static JSONObject parse(String json) {
         return (JSONObject) JSONValue.parse(json);
+    }
+
+    public static String[] toStringArray(JSONArray array) {
+        String[] result = new String[array.size()];
+
+        for (int i = 0; i < array.size(); i++) {
+            result[i] = (String) array.get(i);
+        }
+
+        return result;
     }
 
     public static String stringify(Object object) {
